29583eeaf310e06bd0dd80b5192047fd7fde1266
Dhrubo Arif <ariful48@gmail.com>
2024-06-27 10:14:13 +0600

    update 27 jun

--- resources/views/user/pages/my_assset_mstaking.blade.php
+++ resources/views/user/pages/my_assset_mstaking.blade.php
@@ -2,44 +2,59 @@
 
 
 @section('user_content')
-   <div class="bd-example">
-            <div class="row  row-cols-1 row-cols-md-1 g-4">
-                <div class="col">
-                    <div class="card">
-                    <div class="card-body">
-                        <h4 class="btn btn-success">Simple Earn History</h4>
-                         <h4 class="btn btn-success">Flexible Savings History</h4>
+    <div class="section-admin container-fluid">
+        <div class="row admin">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="admin-content res-mg-t-15 d-flex row justify-content-between">
+
+
+                    <div class="row page-top-section">
+                        <!-- breadcome title Section  -->
+                        <div class="col-sm-6 breadcome-heading">
+                            <h3>Musd Staking History</h3>
+                        </div>
                         
-                        <hr>
-                        <div class="bd-example table-responsive">
-                               <table id="myTable" class="table table-bordered table-border">
-                                   <thead>
-                                       <tr>
-                                           <th scope="col">#</th>
-                                          <th scope="col">AMOUNT</th>
-                                           <th scope="col">DAYS
-                                        </th>
+                    </div>
+
+                  
+
+                    <div class="transection-staus mg-t-30  mg-b-30">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                    <div class="transaction-status-wrap">
+
+                                        <div class="transaction-table">
+                                            <table id="myTable" class="">
+                                                <thead>
+                                                    <tr>
+                                                        <th scope="col">#</th>
+                                                       <th scope="col">PACKAGE</th>
+                                                        <th scope="col">PURCHASE
+                                                             DATE</th>
+             
+             
+                                                        <th scope="col">TOKEN
+                                                          AMOUNT</th>
+                                                        <th scope="col">DAILY
+                                                         BUYER
+                                                         TOKEN</th>
+                                                         <th scope="col">RECEIVED DAYS</th>
+                                                           <th scope="col">REMAINING DAYS</th>
+                                                    </tr>
+                                                </thead>
 
 
-                                           <th scope="col">DAILY BONUS
-                                             </th>
-                                           
-                                            <th scope="col">RECEIVED DAYS</th>
-                                              <th scope="col">REMAINING DAYS</th>
-                                               <th scope="col">STAKING DATE</th>
-                                       </tr>
-                                   </thead>
-                                   <tbody>
-                                     @foreach($purchase as $row)
+                                                @foreach($purchase as $row)
                                        <tr>
                                           <td >{{$loop->index+1}}</td>
-                                          <td>{{$row->amount}}</td>
+                                          <td>{{$row->packages->package_name}}</td>
 
-                                          <td>{{$row->days}}</td>
+                                          <td>{{$row->created_at}}</td>
 
 
-                                            <td>{{round($row->daily,2)}}</td>
-                                           
+                                            <td>{{$row->packages->amount}}</td>
+                                            <td>{{($row->packages->amount)*(($row->packages->daily_buyer_token)/100)}}</td>
                                             <?php
                                             $to = \Carbon\Carbon::createFromFormat('Y-m-d H:s:i',$row->created_at);
                                             $from = \Carbon\Carbon::now();
@@ -53,33 +68,69 @@
                                              ?>
 
 
-                                           
-                                              @if(($row->days)-($diff_in_days)< 0)
-                                            <td>{{$row->days}}</td>
+                                             @if(($row->packages->duration)-($diff_in_days)< 0)
+                                            <td>{{$row->packages->duration}}</td>
                                             <td>0</td>
                                             
                                             @else 
                                             
                                             <td>{{$diff_in_days}}</td>
-                                            <td>{{($row->days)-($diff_in_days)}}</td>
+                                            <td>{{($row->packages->duration)-($diff_in_days)}}</td>
                                             
                                             @endif
-                                            <td>{{$row->created_at}}</td>
+
                                        </tr>
                                        @endforeach
-                                   </tbody>
-                               </table>
-                           </div>
-                    </div>
 
+
+
+
+
+                                                </tbody>
+                                            </table>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                    <!-- transection Token Wallet section
+        ============================================  -->
                 </div>
-
-
             </div>
         </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
 
 
 
+    @push('scripts')
+        <script type="text/javascript">
+            document.getElementById('DestinationOptions_usd2').addEventListener('change', function(e) {
+                var wallet2 = e.target.options[e.target.selectedIndex].getAttribute('id');
+                //console.log(wallet2);
+                var wallet = document.getElementById("wallet_id_usd2").value = wallet2;
+                //console.log(wallet);
+                //wallet.innerHTML= wallet2;
+            });
 
+            //  document.getElementById('').value(id.value);
+        </script>
+
+        <script>
+            document.getElementById('DestinationOptions').addEventListener('change', function(e) {
+                var wallet2 = e.target.options[e.target.selectedIndex].getAttribute('id');
+                console.log(wallet2);
+                var wallet = document.getElementById("wallet_id").value = wallet2;
+                //console.log(wallet);
+                //wallet.innerHTML= wallet2;
+            });
+        </script>
+
+   
+    @endpush
 @endsection
